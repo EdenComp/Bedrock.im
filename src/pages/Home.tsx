ff62f65422fdf4bfb860004d6ff91d3fe23f0399
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2024-02-23 23:02:32 +0100

    refactor: components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,14 @@
-import Layout from "../components/Layout.tsx";
+import Layout from "../components/Landing/Layout.tsx";
 import type { ReactElement } from 'react';
-import Text from "../components/basic/Text.tsx";
+import Text from "../components/Basic/Text.tsx";
 
 export default function Home(): ReactElement {
   return (
     <Layout>
-      <Text className="size-4xl font-bold">Hello, World!</Text>
-      <Text className="size-2xl font-normal">Welcome to your new app.</Text>
-      <Text className="size-2xl font-normal">This is a simple starter template for a React app with TypeScript, Tailwind CSS, and Vite.</Text>
-      <Text className="size-9xl font-bold">HOME</Text>
+      <Text className="font-size-4xl font-bold">Hello, World!</Text>
+      <Text className="font-size-2xl font-normal">Welcome to your new app.</Text>
+      <Text className="font-size-2xl font-normal">This is a simple starter template for a React app with TypeScript, Tailwind CSS, and Vite.</Text>
+      <Text className="font-size-9xl font-bold">HOME</Text>
     </Layout>
   )
 }
