de224387db107a17527118ae845f8d5f6a83add3
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2024-02-24 10:03:09 +0100

    GNEU GNEU, Utilise des fragments, GNEU GNEU

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <>
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/documentation" Component={Documentation} />
         <Route path="/Pricing" Component={Pricing} />
         <Route path="/team" Component={Team} />
       </Routes>
-    </div>
+    </>
   )
 }
 
