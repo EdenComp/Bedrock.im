82de8d71a5e989eca4f89713cfee433832d89512
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2024-02-24 11:05:05 +0100

    fix: fixed the footer even with scrolling

--- src/components/Basic/Footer.tsx
+++ src/components/Basic/Footer.tsx
@@ -2,7 +2,7 @@ import type { ReactElement } from 'react'
 
 export default function Footer(): ReactElement {
   return (
-    <footer className="bg-gray-900 border-t border-gray-600 absolute bottom-0 start-0 w-full z-20">
+    <footer className="bg-gray-900 border-t border-gray-600 fixed bottom-0 left-0 w-full z-20">
       <div className="max-w-screen-xl mx-auto p-4">
         <p className="text-center text-gray-400 text-sm">© 2021 CramptAleph. All rights reserved.</p>
       </div>
