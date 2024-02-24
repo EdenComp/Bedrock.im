fb8b58131df71798cc19958b07844feb47ca0621
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2024-02-24 13:49:54 +0100

    feat: update logo

--- src/components/Landing/Layout/NavBar.tsx
+++ src/components/Landing/Layout/NavBar.tsx
@@ -1,21 +1,37 @@
 import type { ReactElement } from 'react';
 import MetamaskButton from "./Navbar/MetamaskButton.tsx";
 
+const NavBarTab = ({page}: any): ReactElement => {
+  return (
+    <li key={page.name}>
+      <a href={page.href}
+         className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-500 md:p-0 hover:text-white"
+         aria-current="page">
+        {page.name}
+      </a>
+    </li>
+  )
+}
+
 export default function NavBar(): ReactElement {
 
   const pages = [
-    { name: 'Home', href: '/', current: true },
-    { name: 'Documentation', href: '/documentation', current: false },
-    { name: 'Pricing', href: '/pricing', current: false },
-    { name: 'Team', href: '/team', current: false },
+    {name: 'Home', href: '/', current: true},
+    {name: 'Documentation', href: '/documentation', current: false},
+    {name: 'Pricing', href: '/pricing', current: false},
+    {name: 'Team', href: '/team', current: false },
   ]
+
   return (
     <nav className="bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-600">
+
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="/logo.png" className="h-12" alt="Bedrock Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Bedrock.im</span>
         </a>
+
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <MetamaskButton />
           <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
@@ -25,18 +41,15 @@ export default function NavBar(): ReactElement {
             </svg>
           </button>
         </div>
+
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 h-full" id="navbar-sticky">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
             {pages.map((page): ReactElement => (
-              <li key={page.name}>
-                  <a href={page.href} className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-500 md:p-0 hover:text-white"
-                     aria-current="page">
-                    {page.name}
-                  </a>
-              </li>
+              <NavBarTab page={page}/>
             ))}
           </ul>
         </div>
+
       </div>
     </nav>
   )
