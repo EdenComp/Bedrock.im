1de0ddc2b8c306d51db5f20a73e660713e929555
Croos3r <dorian.moy@epitech.eu>
2024-02-24 17:11:47 +0100

    feat(notes-editor): styling and add of note editor
    Took 2 hours 21 minutes
    
    Took 2 seconds

--- src/pages/Documentation.tsx
+++ src/pages/Documentation.tsx
@@ -1,13 +1,14 @@
-import Layout from "../components/Landing/Layout.tsx";
-import type { ReactElement } from 'react';
-import Text from "../components/Basic/Text.tsx";
+import Layout from '../components/Landing/Layout.tsx'
+import type { ReactElement } from 'react'
+import Text from '../components/Basic/Text.tsx'
 
 export default function Documentation(): ReactElement {
   return (
     <Layout>
       <Text className="text-4xl font-bold">Hello, World!</Text>
       <Text className="text-2xl font-normal">Welcome to your new app.</Text>
-      <Text className="text-2xl font-normal">This is a simple starter template for a React app with TypeScript, Tailwind CSS, and Vite.</Text>
+      <Text className="text-2xl font-normal">This is a simple starter template for a React app with TypeScript, Tailwind
+        CSS, and Vite.</Text>
       <Text className="text-9xl font-bold">DOCUMENTATION</Text>
     </Layout>
   )
