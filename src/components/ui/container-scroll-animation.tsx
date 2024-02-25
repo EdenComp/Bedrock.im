63d85b6dfdb7b843757e2cdd7db33e1e835443fa
Tom BP <91875715+Tomi-Tom@users.noreply.github.com>
2024-02-25 01:39:50 +0100

    Feat/app product (#13)
    * refactor: change logo
    
    * feat: base notetree for the app
    
    * feat: v1 editeur all link
    
    * feat: refactor functional editor
    
    * fix: data
    
    * fix: some build
    
    ---------
    
    Co-authored-by: Florian Lauch <florian.lauchh@gmail.com>

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -1,16 +1,8 @@
 "use client";
 import React, { useRef } from "react";
 import { useScroll, useTransform, motion } from "framer-motion";
-export const ContainerScroll = ({
-                                  users,
-                                  titleComponent,
-                                }: {
-  users: {
-    name: string;
-    designation: string;
-    image: string;
-    badge?: string;
-  }[];
+import Notes from "../../components/Landing/Home/Notes.tsx";
+export const ContainerScroll = ({titleComponent,}: {
   titleComponent: string | React.ReactNode;
 }) => {
   const containerRef = useRef<any>(null);
@@ -54,7 +46,6 @@ export const ContainerScroll = ({
           rotate={rotate}
           translate={translate}
           scale={scale}
-          users={users}
         />
       </div>
     </div>
@@ -78,53 +69,25 @@ export const Card = ({
                        rotate,
                        scale,
                        translate,
-                       users,
                      }: {
   rotate: any;
   scale: any;
   translate: any;
-  users: {
-    name: string;
-    designation: string;
-    image: string;
-    badge?: string;
-  }[];
 }) => {
   return (
     <motion.div
       style={{
-        rotateX: rotate, // rotate in X-axis
+        rotateX: rotate,
         scale,
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
-      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-6 bg-[#222222] rounded-[30px] shadow-2xl"
+      className="max-w-4xl mx-auto h-[20rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-6 bg-[#222222] rounded-[30px] shadow-2xl"
     >
-      <div className="bg-gray-100 h-full w-full rounded-2xl grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 overflow-hidden p-4">
-        {users.map((user, idx: number) => (
-          <motion.div
-            key={`user-${idx}`}
-            className="bg-white rounded-md cursor-pointer relative"
-            style={{ translateY: translate }}
-            whileHover={{
-              boxShadow:
-                "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
-            }}
-          >
-            <div className="absolute top-2 right-2 rounded-full text-xs font-bold bg-white px-2 py-1">
-              {user.badge}
-            </div>
-            <img
-              src={user.image}
-              className="rounded-tr-md rounded-tl-md text-sm "
-              alt="thumbnail"
-            />
-            <div className="p-4">
-              <h1 className="font-semibold text-sm ">{user.name}</h1>
-              <h2 className=" text-gray-500 text-xs ">{user.designation}</h2>
-            </div>
+      <div className="bg-background-2 h-full w-full rounded-2xl overflow-hidden">
+          <motion.div style={{ translateY: translate, rotateX: rotate, scale }} className="col-span-1 md:col-span-3 lg:col-span-5">
+            <Notes />
           </motion.div>
-        ))}
       </div>
     </motion.div>
   );
